99628a758cec6dec4710f235f7c907e714cf4729
laffed <roarklaffed@gmail.com>
2020-02-11 16:52:48 -0600

    added bio, minor css edits

--- index.js
+++ index.js
@@ -9,8 +9,14 @@ function renderPage() {
   //render user handle
   document.getElementById("userHandle").innerText = "@" + retrievedObject.login;
   document.getElementById("userHandle").href = retrievedObject.html_url;
+  //render bio
+  document.getElementById("userBio").innerHTML = retrievedObject.bio
   //render date joined
-  let options = { month: "long", day: "numeric", year: "numeric" };
+  let options = {
+    month: "long",
+    day: "numeric",
+    year: "numeric"
+  };
   document.getElementById("dateJoined").innerHTML = retrievedObject.created_at;
   //render repositories integer
   document.getElementById("repos").innerText = retrievedObject.public_repos;
